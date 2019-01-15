b0c62052c0c37e267e5510ad20d819c378f887a5
Tim de Bruin <t.d.debruin@tudelft.nl>
2019-01-15 17:18:15 +0100

    import relevant classes when importing package

--- cor_control_benchmarks/__init__.py
+++ cor_control_benchmarks/__init__.py
@@ -1,2 +1,7 @@
+from cor_control_benchmarks.pendulum import PendulumBenchmark
+from cor_control_benchmarks.magman import MagmanBenchmark
+from cor_control_benchmarks.segway import SegwayBenchmark
+from cor_control_benchmarks.control_benchmark import RewardType, DomainBound
+
 name = "cor_control_benchmarks"
 __all__ = ["control_benchmark", "magman", "segway", "pendulum", "result_logging"]
